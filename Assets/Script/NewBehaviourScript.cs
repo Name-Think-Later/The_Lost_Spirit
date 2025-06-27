20d92e8f257b5e08775d9508daf9ced7b13e2347
ukaba05 <ukaba05@gmail.com>
2025-06-27 12:15:10 +0800

    Init

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+using MoreLinq;
+using UnityEngine;
+using ZLinq;
+
+public class NewBehaviourScript : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
