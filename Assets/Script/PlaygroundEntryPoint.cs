5e67650a39424d7b7ccd15616f493fb2e99ba710
ukaba05 <ukaba05@gmail.com>
2025-07-03 05:11:48 +0800

    Add PlayerReference

--- Assets/Script/PlaygroundEntryPoint.cs
+++ Assets/Script/PlaygroundEntryPoint.cs
@@ -1,8 +1,11 @@
-using System;
 using UnityEngine;
 
 public class PlaygroundEntryPoint : MonoBehaviour {
+    [SerializeField] PlayerReference _playerReference;
+
+    ActionMap _actionMap;
+
     void Awake() {
-        
+        _actionMap = new ActionMap();
     }
 }
