78090bc7e3eae17b43b087e95165b36291655d4e
ukaba05 <ukaba05@gmail.com>
2025-07-08 18:14:03 +0800

    Import NSubstitute for UnitTest mocking Add CircuitNodeTest

---
+++ Assets/Script/TheLostSpirit/Tests/CircuitNodeTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using Script.TheLostSpirit.Circuit;
+
+
+namespace Script.TheLostSpirit.Tests {
+    public class CircuitNodeTest
+    {
+        CircuitNode _nodeA, _nodeB, _nodeC;
+        
+        [SetUp]
+        public void Setup()
+        {
+                
+        }
+    
+        [Test]
+        public void UnitTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
