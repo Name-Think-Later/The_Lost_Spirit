f719dadfed19e876ae10c8f3c261238f8f563f81
Nep <koan050401@gmail.com>
2025-10-06 18:50:59 +0800

    backpack拖曳操作

--- Assets/Script/UI/Drag.cs
+++ Assets/Script/UI/Drag.cs
@@ -4,36 +4,84 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-public class Drag : MonoBehaviour, IDragHandler
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private RectTransform rectTransform;
-    private RectTransform parentRectTransform;
+    private Canvas canvas;
+    private CanvasGroup canvasGroup;
+    private Vector2 originalPosition;
+    private Transform originalParent;
+    private Vector2 dragOffset;
+
+    public GameObject mainNodePrefab;
+    public GameObject backpackNodePrefab;
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
-        parentRectTransform = transform.parent.GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null) canvasGroup = gameObject.AddComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalPosition = rectTransform.anchoredPosition;
+        originalParent = transform.parent;
+        canvasGroup.blocksRaycasts = false;
+        // 計算滑鼠點擊時與物件中心的 offset
+        Vector2 localMousePos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, eventData.pressEventCamera, out localMousePos);
+        dragOffset = rectTransform.anchoredPosition - localMousePos;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        // 計算拖曳後的新位置
-        Vector2 newPos = rectTransform.anchoredPosition + eventData.delta;
+        // 跟隨滑鼠移動，考慮 offset
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, eventData.pressEventCamera, out pos);
+        rectTransform.anchoredPosition = pos + dragOffset;
+    }
 
-        // Panel的尺寸
-        Vector2 panelSize = parentRectTransform.rect.size;
-        Vector2 nodeSize = rectTransform.rect.size;
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
 
-        // 限制X座標
-        float minX = -panelSize.x / 2 + nodeSize.x / 2 + 20;
-        float maxX = panelSize.x / 2 - nodeSize.x / 2 - 20;
-        newPos.x = Mathf.Clamp(newPos.x, minX, maxX);
+        // 檢查拖曳目標
+        if (eventData.pointerEnter == null)
+        {
+            // 拖到無效區域，回到原本位置與父物件
+            transform.SetParent(originalParent);
+            rectTransform.anchoredPosition = originalPosition;
+            return;
+        }
 
-        // 限制Y座標
-        float minY = -panelSize.y / 2 + nodeSize.y / 2 + 20;
-        float maxY = panelSize.y / 2 - nodeSize.y / 2 - 20;
-        newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
-
-        rectTransform.anchoredPosition = newPos;
+        // 只允許拖到 backpack 或 NodeContainer
+        if (eventData.pointerEnter.CompareTag("Backpack"))
+        {
+            // 拖到 backpack，生成 backpack_node在滑鼠位置
+            GameObject newNode = Instantiate(backpackNodePrefab, eventData.pointerEnter.transform);
+            RectTransform newRect = newNode.GetComponent<RectTransform>();
+            Vector2 localPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(eventData.pointerEnter.transform as RectTransform, eventData.position, eventData.pressEventCamera, out localPos);
+            newRect.anchoredPosition = localPos;
+            Destroy(gameObject);
+        }
+        else if (eventData.pointerEnter.CompareTag("NodeContainer"))
+        {
+            // 拖到 NodeContainer，生成 main_node在滑鼠位置
+            GameObject newNode = Instantiate(mainNodePrefab, eventData.pointerEnter.transform);
+            RectTransform newRect = newNode.GetComponent<RectTransform>();
+            Vector2 localPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(eventData.pointerEnter.transform as RectTransform, eventData.position, eventData.pressEventCamera, out localPos);
+            newRect.anchoredPosition = localPos;
+            Destroy(gameObject);
+        }
+        else
+        {
+            // 拖到其他地方都回原位
+            transform.SetParent(originalParent);
+            rectTransform.anchoredPosition = originalPosition;
+        }
     }
 }
